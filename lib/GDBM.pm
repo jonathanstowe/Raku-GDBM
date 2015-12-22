0828359d557fb3df9bbddeb86c7acd54f2304333
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-12-22 10:29:10 +0000

    Add Datum

--- lib/GDBM.pm
+++ lib/GDBM.pm
@@ -1,5 +1,12 @@
 use v6;
 
+use NativeCall;
+
 class GDBM does Associative {
+    my class Datum is repr('CStruct') {
+        has Str $.dptr is rw;
+        has int $.dsize is rw;
+
+    }
 }
 # vim: ft=perl6 expandtab sw=4
