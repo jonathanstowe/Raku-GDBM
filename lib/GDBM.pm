ebb6e8c6017c57a1fc84312997c1028e35035bb9
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-12-22 17:59:12 +0000

    open

--- lib/GDBM.pm
+++ lib/GDBM.pm
@@ -3,9 +3,19 @@ use v6;
 use NativeCall;
 
 class GDBM does Associative {
+    enum OpenMode ( Reader => 0, Writer => 1, Create => 2, New => 3);
+    my constant OpenMask = 7;
+    enum OpenOptions ( Fast => 0x010, Sync => 0x020, NoLock => 0x040, NoMMap => 0x080, CloExec => 0x100);
+    
+    enum StoreOptions ( Insert => 0, Replace => 1 );
+
     my class Datum is repr('CStruct') {
         has Str $.dptr is rw;
         has int $.dsize is rw;
+    }
+
+    my class File is repr('CPointer') {
+        sub gdbm_open(Str $file, int $bs, int $flags, int $mode, &fatal ( Str $message)) returns File is native('libgdbm') { * }
 
     }
 }
