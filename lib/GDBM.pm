e9c663571924f95757497a71ae5308bd4f44c125
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-12-23 10:41:30 +0000

    Add most of the methods (untested)

--- lib/GDBM.pm
+++ lib/GDBM.pm
@@ -12,6 +12,15 @@ class GDBM does Associative {
     my class Datum is repr('CStruct') {
         has Str $.dptr is rw;
         has int $.dsize is rw;
+
+        multi method new(Str $val) {
+            my int $dsize = $val.encode.bytes;
+            self.new(dptr => $val, :$dsize);
+        }
+
+        method Str() {
+            $!dptr;
+        }
     }
 
     class X::Fatal is Exception {
@@ -30,6 +39,79 @@ class GDBM does Associative {
 
         }
 
+        sub gdbm_close(File $f) is native('libgdbm') { * };
+
+        method close() {
+            gdbm_close(self);
+        }
+
+        sub gdbm_store(File $f, Datum $k, Datum $v, int $m) returns int is native('libgdbm') { * }
+
+        multi method store(Datum $k, Datum $v, StoreOptions $flag = Replace) returns Int {
+            gdbm_store(self, $k, $v, $flag.Int);
+        }
+
+        multi method store(Str $k, Str $v, StoreOptions $flag = Replace) returns Int {
+            self.store(Datum.new($k), Datum.new($v), $flag);
+        }
+
+        sub gdbm_fetch(File $f, Datum $k) returns Datum is native('libgdbm') { * }
+
+        multi method fetch(Datum $k) returns Str {
+            my $ret = gdbm_fetch(self, $k);
+            $ret.Str;
+        }
+
+        multi method fetch(Str $k) returns Str {
+            self.fetch(Datum.new($k));
+        }
+
+        sub gdbm_delete(File $f, Datum $k) returns int is native('libgdbm') { * }
+        multi method delete(Datum $k) returns Int {
+            gdbm_delete(self, $k);
+        }
+
+        multi method delete(Str $k) returns Int {
+            self.delete(Datum.new($k));
+        }
+
+        # For the methods of these we'll just return the Datum as
+        # we'll only be passing to next anyway
+
+        sub gdbm_firstkey(File $f) returns Datum is native('libgdbm') { * }
+
+        multi method first-key() returns Datum {
+            gdbm_firstkey(self);
+        }
+
+
+        sub gdbm_nextkey(File $f, Datum $prev) returns Datum is native('libgdbm') { * }
+
+        multi method next-key(Datum $prev) returns Datum {
+            gdbm_nextkey(self, $prev);
+        }
+
+        sub gdbm_reorganize (File $f) returns int is native('libgdbm') { * }
+
+        method reorganize() returns Int {
+            gdbm_reorganize(self);
+        }
+
+        sub gdbm_sync(File $f) is native('libgdbm') { * }
+
+        method sync() {
+            gdbm_sync(self);
+        }
+
+        sub gdbm_exists(File $f, Datum $k) returns int is native('libgdbm') { * }
+        multi method exists(Datum $k) returns Bool {
+            my Int $rc = gdbm_exists(self, $k);
+            return Bool($rc);
+        }
+
+        multi method exists(Str $k) returns Bool {
+            self.exists(Datum.new($k));
+        }
     }
 }
 # vim: ft=perl6 expandtab sw=4
