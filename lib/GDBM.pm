ba68b795402715d896a9f79a6260cb8b0881e76a
Jonathan Stowe <jns+git@gellyfish.co.uk>
2016-03-30 13:09:41 +0100

    Try with a wrapper

--- lib/GDBM.pm
+++ lib/GDBM.pm
@@ -4,6 +4,9 @@ use NativeCall;
 use NativeHelpers::Array;
 
 class GDBM does Associative {
+
+    my constant HELPER = %?RESOURCES<libraries/gdbmhelper>.Str;
+
     enum OpenMode ( Reader => 0, Writer => 1, Create => 2, New => 3);
     my constant OpenMask = 7;
     enum OpenOptions ( Fast => 0x010, Sync => 0x020, NoLock => 0x040, NoMMap => 0x080, CloExec => 0x100);
@@ -11,26 +14,19 @@ class GDBM does Associative {
     enum StoreOptions ( Insert => 0, Replace => 1 );
 
     class Datum is repr('CStruct') is rw {
-        has CArray           $.dptr  is rw;
-        has uint32           $.dsize is rw;
+        has str              $.dptr;
+        has uint32           $.dsize;
 
         multi method new(Str() $val) {
             my $buf = $val.encode; 
             my uint32 $dsize = $buf.bytes;
-            my $a = self.new(dptr => $buf, :$dsize);
-            $a.dsize = $dsize;
+            my $a = self.bless(dptr => $val, :$dsize);
             return $a;
         }
 
-        submethod BUILD(:$dptr, :$dsize) {
-
-            my $a:= copy-buf-to-carray(Buf.new($dptr.list));
-            $!dptr := $a;
-            $!dsize = $dsize;
-        }
-
         method Str() {
-            copy-carray-to-buf($!dptr, $!dsize).decode;
+            say "Str";
+            Str($!dptr);
         }
     }
 
@@ -44,25 +40,25 @@ class GDBM does Associative {
     }
 
     my class File is repr('CPointer') {
-        sub gdbm_open(Str $file, uint32 $bs, uint32 $flags, uint32 $mode, &fatal ( Str $message )) returns File is native('gdbm',v4) { * }
+        sub p_gdbm_open(Str $file, uint32 $bs, uint32 $flags, uint32 $mode, &fatal ( Str $message )) returns File is native(HELPER) { * }
 
         multi method new(Str() :$file, Int :$block-size = 512, Int() :$flags = Create +| Sync +| NoMMap, Int :$mode = 0o644) returns File {
             explicitly-manage($file);
-            gdbm_open($file, $block-size, $flags, $mode, &fail);
+            p_gdbm_open($file, $block-size, $flags, $mode, &fail);
 
         }
 
-        sub gdbm_close(File $f) is native('gdbm',v4) { * };
+        sub p_gdbm_close(File $f) is native(HELPER) { * };
 
         method close() {
-            gdbm_close(self);
+            p_gdbm_close(self);
         }
 
-        sub gdbm_store(File:D $f, Datum $k, Datum $v, uint32 $m) returns int32 is native('gdbm',v4) { * }
+        sub p_gdbm_store(File:D $f, Datum $k, Datum $v, uint32 $m) returns int32 is native(HELPER) { * }
 
         multi method store(Datum:D $k, Datum:D $v, StoreOptions $flag = Replace) returns Int {
             say "store { $k.perl } => { $v.perl }";
-            gdbm_store(self, $k, $v, $flag.Int);
+            p_gdbm_store(self, $k, $v, $flag.Int);
         }
 
         multi method store(Str:D $k, Str:D $v, StoreOptions $flag = Replace) returns Int {
@@ -72,10 +68,11 @@ class GDBM does Associative {
             self.store($key, $val, $flag);
         }
 
-        sub gdbm_fetch(File $f, Datum $k) returns Datum is native('gdbm',v4) { * }
+        sub p_gdbm_fetch(File $f, Datum $k) returns Datum is native(HELPER) { * }
 
         multi method fetch(Datum $k) returns Str {
-            my $ret = gdbm_fetch(self, $k);
+            my $ret = p_gdbm_fetch(self, $k);
+            say $ret;
             $ret.Str;
         }
 
@@ -83,9 +80,9 @@ class GDBM does Associative {
             self.fetch(Datum.new($k));
         }
 
-        sub gdbm_delete(File $f, Datum $k) returns int32 is native('gdbm',v4) { * }
+        sub p_gdbm_delete(File $f, Datum $k) returns int32 is native(HELPER) { * }
         multi method delete(Datum $k) returns Int {
-            gdbm_delete(self, $k);
+            p_gdbm_delete(self, $k);
         }
 
         multi method delete(Str $k) returns Int {
@@ -95,34 +92,34 @@ class GDBM does Associative {
         # For the methods of these we'll just return the Datum as
         # we'll only be passing to next anyway
 
-        sub gdbm_firstkey(File $f) returns Datum is native('gdbm',v4) { * }
+        sub p_gdbm_firstkey(File $f) returns Datum is native(HELPER) { * }
 
         multi method first-key() returns Datum {
-            gdbm_firstkey(self);
+            p_gdbm_firstkey(self);
         }
 
 
-        sub gdbm_nextkey(File $f, Datum $prev) returns Datum is native('gdbm',v4) { * }
+        sub p_gdbm_nextkey(File $f, Datum $prev) returns Datum is native(HELPER) { * }
 
         multi method next-key(Datum $prev) returns Datum {
-            gdbm_nextkey(self, $prev);
+            p_gdbm_nextkey(self, $prev);
         }
 
-        sub gdbm_reorganize (File $f) returns int32 is native('gdbm',v4) { * }
+        sub p_gdbm_reorganize (File $f) returns int32 is native(HELPER) { * }
 
         method reorganize() returns Int {
-            gdbm_reorganize(self);
+            p_gdbm_reorganize(self);
         }
 
-        sub gdbm_sync(File $f) is native('gdbm',v4) { * }
+        sub p_gdbm_sync(File $f) is native(HELPER) { * }
 
         method sync() {
-            gdbm_sync(self);
+            p_gdbm_sync(self);
         }
 
-        sub gdbm_exists(File $f, Datum $k) returns int32 is native('gdbm',v4) { * }
+        sub p_gdbm_exists(File $f, Datum $k) returns int32 is native(HELPER) { * }
         multi method exists(Datum $k) returns Bool {
-            my Int $rc = gdbm_exists(self, $k);
+            my Int $rc = p_gdbm_exists(self, $k);
             return Bool($rc);
         }
 
@@ -130,12 +127,12 @@ class GDBM does Associative {
             self.exists(Datum.new($k));
         }
 
-        sub gdbm_count(File $f, CArray[uint64] $pcount) returns int32 is native('gdbm',v4) { * }
+        sub p_gdbm_count(File $f, CArray[uint64] $pcount) returns int32 is native(HELPER) { * }
 
         method count() returns Int {
             my CArray[uint64] $pcount = CArray[uint64].new;
             $pcount[0] = 0;
-            gdbm_count(self, $pcount);
+            p_gdbm_count(self, $pcount);
             return Int($pcount[0]);
         }
 
